920f3e8f8f5039b39a49fa7fd527b8429c37f191
Martin Schön <martin.schoen@spiegel.de>
2024-04-12 17:05:01 +0200

    Added sub-directives x-editable:disabled and x-editable:suspended for toggling disabled or suspended state

--- src/index.js
+++ src/index.js
@@ -14,25 +14,100 @@ events.forEach((eventName) => {
     });
 });
 
-export default function (Alpine) {
-    Alpine.directive("editable", (el, { expression }, { cleanup, evaluateLater, effect }) => {
-        // add to editable
-        editable.add(el);
-        // setup reactivity
-        const getValue = evaluateLater(expression);
-        effect(() => {
-            getValue((value) => {
-                if (document.activeElement === el) {
-                    return;
-                }
-                const targetvalue = value || "";
-                if (el.innerHTML !== targetvalue) {
-                    el.innerHTML = targetvalue;
-                }
-            });
+let elementConfigs = [];
+
+function getElementConfig(el) {
+    if (!(el instanceof Element)) {
+        return null;
+    }
+    
+    const existingConfig = elementConfigs.find((config) => config.el === el);
+    if (existingConfig) {
+        return existingConfig;
+    }
+
+    const newConfig = { el, disabled: false, suspended: false };
+    elementConfigs.push(newConfig);
+    return newConfig;
+}
+
+function initEditableElement(el, expression, utilities) {
+    const { cleanup, evaluateLater, effect } = utilities;
+    const elementConfig = getElementConfig(el);
+    
+    // register element with editable.js
+    editable.add(el);
+    if (elementConfig.disabled) {
+        editable.disable(el);
+    }
+    if (elementConfig.suspended) {
+        editable.suspend(el);
+    }
+    
+    // setup reactivity
+    const getValue = evaluateLater(expression);
+    effect(() => {
+        getValue((value) => {
+            if (document.activeElement === el) {
+                return;
+            }
+            const targetvalue = value || "";
+            if (el.innerHTML !== targetvalue) {
+                el.innerHTML = targetvalue;
+            }
         });
-        // cleanup
-        cleanup(() => editable.remove(el));
+    });
+    
+    // cleanup
+    cleanup(() => {
+        editable.remove(el);
+        elementConfigs = elementConfigs.filter((config) => config !== elementConfig);
+    });
+}
+
+function bindDisabled(el, expression, utilities) {
+    const { evaluateLater, effect } = utilities;
+    const elementConfig = getElementConfig(el);
+    const getDisabled = evaluateLater(expression);
+    effect(() => {
+        getDisabled((disabled) => {
+            if (disabled) {
+                editable.disable(el);
+            } else {
+                editable.enable(el);
+            }
+            elementConfig.disabled = disabled;
+        });
+    });
+}
+
+function bindSuspended(el, expression, utilities) {
+    const { evaluateLater, effect } = utilities;
+    const elementConfig = getElementConfig(el);
+    const getSuspended = evaluateLater(expression);
+    effect(() => {
+        getSuspended((suspended) => {
+            if (suspended) {
+                editable.suspend(el);
+            } else {
+                editable.continue(el);
+            }
+            elementConfig.suspended = suspended;
+        });
+    });
+}
+
+export default function (Alpine) {
+    Alpine.directive("editable", (el, { value, expression }, utilities) => {
+        if (value === 'disabled') {
+            bindDisabled(el, expression, utilities);
+        } else if (value === 'suspended') {
+            bindSuspended(el, expression,  utilities);
+        } else if (!value) {
+            initEditableElement(el, expression, utilities);
+        } else {
+            console.warn(`[alpine-editable] unknown directive x-editable:${value}`);
+        }
     });
     Alpine.magic("editable", () => editable);
 }
